62fc90d353c611406c93238c1b6236566c869d91
nathdub <167571824+nathdub@users.noreply.github.com>
2025-09-07 16:54:35 +0200

    changelog initial commit

---
+++ src/routes/changelog/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Changelog from '../../changelog.svx';
+</script>
+
+<h1>nathdub.com - changelog</h1>
+<div class="changelog">
+	<Changelog />
+</div>
+
+<style>
+	.changelog {
+		max-width: 800px;
+		margin: 0 auto;
+		padding: 2rem;
+		font-family: 'Segoe UI', sans-serif;
+		color: #333;
+	}
+</style>
