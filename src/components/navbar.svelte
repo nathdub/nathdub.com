5d9d9c8e98496d4c229e574d7aca6d01b0100b58
nathdub <nathdub@proton.me>
2025-09-07 15:46:31 +0200

    navbar initial commit

---
+++ src/components/navbar.svelte
@@ -0,0 +1,35 @@
+<header>
+	<nav>
+		<a class="nav-link" href="/">Home</a>
+		<a class="nav-link" href="/changelog/">Changelog</a>
+		<a class="nav-link" href="https://forms.gle/hP9qgyb9x2j5U38PA">Feedback</a>
+	</nav>
+	<span class="dub-version">nathdub.com - 3.0.0</span>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		background-color: teal;
+		padding: 0.4rem 1rem;
+	}
+
+	nav {
+		display: flex;
+		gap: 1rem;
+	}
+
+	.nav-link {
+		color: white;
+		text-decoration: none;
+	}
+
+	.nav-link:hover {
+		color: yellow;
+	}
+
+	.dub-version {
+		color: rgb(255, 137, 220);
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import Navbar from '../components/navbar.svelte';
 
 	let { children } = $props();
 </script>
@@ -8,4 +9,19 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<Navbar/>
+
+<main>
+	{@render children?.()}
+</main>
+
+<style>
+	:global(body) {
+		font-family: sans-serif;
+		margin: 0;
+	}
+
+	main {
+		padding: 0 1rem;
+	}
+</style>
