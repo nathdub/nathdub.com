b649ef67369c857d0684af00e841fc21a2ce7e39
nathdub <167571824+nathdub@users.noreply.github.com>
2025-09-07 16:02:33 +0200

    home page initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div>
+	<h1>Bonjour, ici dub ! Soyez les bienvenus sur le site !</h1>
+	<ul>
+		<li><a class="home-link" href="MarksCalculator/">Marks Calculator</a></li>
+		<li><a class="home-link" href="SoundBoard/">Sound Board</a></li>
+		<li><a class="home-link" href="Calculator/">Calculator</a></li>
+		<li>
+			<a class="home-link" href="https://fr.wikipedia.org/wiki/Dub_(homonymie)" target="_blank" rel="noopener noreferrer">Follow me on Wikipedia</a>
+		</li>
+		<li><a class="home-link" href="FancyMessage/">Fancy Message</a></li>
+		<li><a class="home-link" href="LetMeSearchThisForYou/">Let Me Search This For You</a></li>
+		<li><a class="home-link" href="CrazyPaint/">Crazy Paint</a></li>
+		<li><a class="home-link" href="DubTweaks/">dub Tweaks Browser Extension</a></li>
+		<li><a class="home-link" href="Luciva/">Luciva Productivity Browser Extension</a></li>
+	</ul>
+</div>
+
+<style>
+	.home-link {
+		color: brown;
+		display: inline-block;
+		transition: ease-in-out 0.25s;
+	}
+
+	.home-link:hover {
+		color: red;
+		transform-origin: left;
+		transform: translateX(10px) scale(1.25);
+	}
+</style>
