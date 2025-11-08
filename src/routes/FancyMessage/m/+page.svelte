de0318e96441920449df3b72276dc5d191d319c8
nathdub <167571824+nathdub@users.noreply.github.com>
2025-11-08 14:18:06 +0100

    fancymessage port to svelte

---
+++ src/routes/FancyMessage/m/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import FancyMessageDisplay from '$lib/components/fancy-message-display.svelte';
+
+	let { data }: { data: PageData } = $props();
+
+	$effect(() => {
+		document.body.classList.add('fancymessage-theme');
+		return () => document.body.classList.remove('fancymessage-theme');
+	});
+</script>
+
+<div id="back-link"><a href="/FancyMessage/">Fancy Message</a></div>
+
+<FancyMessageDisplay
+	text={data.text}
+	font={data.font}
+	color={data.color}
+	size={data.size}
+	leftMargin={data.leftMargin}
+	topMargin={data.topMargin}
+	bold={data.bold}
+	italic={data.italic}
+	rainbow={data.rainbow}
+	animated={data.animated}
+/>
+
+<style>
+	:global(.fancymessage-theme) {
+		--bg-color: white;
+		--bg-color-2: rgb(233, 233, 233);
+		--bg-color-3: rgb(214, 214, 214);
+		--text-color: #131313;
+	}
+</style>
